5469f9ee321b18d0f42cc6675f44c6be544d9758
Jejen Jaenudin <1125170031@global.ac.id>
2025-11-08 10:12:07 +0700

    update spals_screen_3

--- lib/screen/splash_screen_3.dart
+++ lib/screen/splash_screen_3.dart
@@ -18,88 +18,87 @@ class MySplashScreen3 extends StatelessWidget {
                 color: Colors.green,
                 shape: BoxShape.circle,
                 image: DecorationImage(
-                    image: AssetImage("assets/images/test3.jpeg"),
-                    fit: BoxFit.cover,
-                  ),
+                  image: AssetImage("assets/images/test3.jpeg"),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-            SizedBox(height: 25,),
+            SizedBox(height: 25),
             Text(
               "welcome 3",
               style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 5),
-              Text(
-                "Aplikasi dompet digital \n yang membuat anda mudah dalam transaksi",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.greenAccent,
-                ),
+            Text(
+              "Aplikasi dompet digital \n yang membuat anda mudah dalam transaksi",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.greenAccent,
               ),
-                SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 10,
-                    width: 10,
-                    decoration: const BoxDecoration(
-                      color: Color.fromARGB(255, 211, 219, 215),
-                      shape: BoxShape.circle,
-                    ),
-                  ),
-                  SizedBox(width: 10),
-                  Container(
-                    height: 10,
-                    width: 10,
-                    decoration: const BoxDecoration(
-                      color: Color.fromARGB(255, 211, 219, 215),
-                      shape: BoxShape.circle,
-                    ),
-                  ),
-                  SizedBox(width: 10),
-                  Container(
-                    height: 10,
-                    width: 10,
-                    decoration: const BoxDecoration(
-                      color: Color.fromARGB(255, 7, 243, 109),
-                      shape: BoxShape.circle,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20),
-              Container(
-                margin: EdgeInsets.only(left: 40, right: 40),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 40,
-                  child: ElevatedButton(
-                    onPressed: () {
-                     Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => Login(),
-                        ),
-                      );
-                    },
-                    //ElevatedButton digunakan untuk membuat lingkaran pada text
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
-                    ),
-                    child: Text(
-                      "welcome 3",
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 10,
+                  width: 10,
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 211, 219, 215),
+                    shape: BoxShape.circle,
                   ),
                 ),
+                SizedBox(width: 10),
+                Container(
+                  height: 10,
+                  width: 10,
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 211, 219, 215),
+                    shape: BoxShape.circle,
+                  ),
+                ),
+                SizedBox(width: 10),
+                Container(
+                  height: 10,
+                  width: 10,
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 7, 243, 109),
+                    shape: BoxShape.circle,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Container(
+              margin: EdgeInsets.only(left: 40, right: 40),
+              child: SizedBox(
+                width: double.infinity,
+                height: 40,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => Login()),
+                      (Route<dynamic> route) => false,
+                    );
+                  },
+                  //ElevatedButton digunakan untuk membuat lingkaran pada text
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                  ),
+                  child: Text(
+                    "welcome 3",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
               ),
+            ),
           ],
         ),
       ),
